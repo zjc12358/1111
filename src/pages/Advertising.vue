902f321149ac146974878573e159bac056d194b5
zjc <710124011@qq.com>
2019-03-21 09:37:17 +0800

    2

--- src/pages/Advertising.vue
+++ src/pages/Advertising.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <!--Advertising-->
-    {{ selected }}
-    <div @click="fn">1212121</div>
+    <!--{{ selected }}-->
+    <div>
+      <div class="modal_mask">
+      </div>
+      <div class="modal_container">
+        <div class="modal_title">选择分类</div>
+      </div>
+    </div>
+    <mt-button @click="fn" size="large">点击上拉 action sheet</mt-button>
     <tabbar :selected="selected" :tabs='tabs'></tabbar>
   </div>
 </template>
@@ -18,7 +25,8 @@
       data(){
         return {
           selected: "GroupList",
-          tabs: this.$store.state.tabs
+          tabs: this.$store.state.tabs,
+          modalShow: false
         }
       },
       created () {
@@ -29,12 +37,32 @@
       },
       methods: {
         fn(){
-          console.log('我是fn')
+          this.modalShow = !this.modalShow
         }
       }
     }
 </script>
 
 <style scoped>
+  .modal_mask{
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 9999;
+    background: black;
+    opacity: 0.6;
+  }
+  .modal_container{
+    background: white;
+    position: relative;
+    z-index: 10000;
+    border-radius: 10px;
+    font-size: 4.2vw;
+    min-height: 100px;
+  }
+  .modal_title{
 
+  }
 </style>
