a94e086945a375ed4e2446c865bb7e90b6a6bf2f
zjc <710124011@qq.com>
2019-03-29 16:36:26 +0800

    添加拦截器判断token. 我的团队.

--- src/main.js
+++ src/main.js
@@ -30,43 +30,45 @@ new Vue({
 })
 
 // 登录路由拦截
-// router.beforeEach((to, from, next) => {
-//   if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
-//     if (store.state.token) {  // 通过vuex state获取当前的token是否存在
-//       next();
-//     }
-//     else {
-//       next({
-//         path: '/login',
-//         // query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
-//       })
-//     }
-//   }
-//   else {
-//     next();
-//   }
-// })
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
+    if (store.state.token) {  // 通过vuex state获取当前的token是否存在
+      next();
+    }
+    else {
+      Indicator.close();
+      Toast('登录过期,请重新登录!')
+      next({
+        path: '/login',
+        // query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
+      })
+    }
+  }
+  else {
+    next();
+  }
+})
 
 /* 响应拦截器 */
 
-axios.interceptors.response.use(function (response) {
-
-  if (response.data.msg === 'token失效') {
-
-    // Storage.localRemove('token') // 删除已经失效或过期的token（不删除也可以，因为登录后覆盖）
-    Indicator.close();
-    Toast('登录过期,请重新登录!')
-    router.replace({
-    path: '/login' // 到登录页重新获取token
-
-  })
-
-  }
-
-  return response
-
-}, function (error) {
-
-  return Promise.reject(error)
-})
+// axios.interceptors.response.use(function (response) {
+//
+//   if (response.data.msg === 'token失效') {
+//
+//     // Storage.localRemove('token') // 删除已经失效或过期的token（不删除也可以，因为登录后覆盖）
+//     Indicator.close();
+//     Toast('登录过期,请重新登录!')
+//     router.replace({
+//     path: '/login' // 到登录页重新获取token
+//
+//   })
+//
+//   }
+//
+//   return response
+//
+// }, function (error) {
+//
+//   return Promise.reject(error)
+// })
 
